874a11f7dd001e506674360b2027873fdb9cb9ed
Pont Lurcock <pont@talvi.net>
2016-08-31 13:32:05 +0200

    Put the formatted citation on the clipboard
    qbibformat now uses xclip to put the citation on the X clipboard.
    Some improvement still needed -- there's some extra text and
    formatting added to the clipboard content.

--- qbibformat.py
+++ qbibformat.py
@@ -4,30 +4,39 @@ import sys
 import os
 import pexpect
 import subprocess
-from subprocess import PIPE
+from subprocess import PIPE, DEVNULL, Popen
 
 def main():
 
-  p = subprocess.Popen(["bibtool", "-R",
-                        "--", "select {$key \"%s\"}" % sys.argv[1],
-                        "--", "delete.field { abstract }",
-                        "--", "delete.field { mynote }",
-                        "--", "delete.field { url }",
-                        "~/files/mine/text/bibliography/geoscience.bib",
-                        "-o", "temp.bib"
-                      ])
-  p.wait()
+  bibtool = Popen(
+    ["bibtool", "-R",
+     "--", "select {$key \"%s\"}" % sys.argv[1],
+     "--", "delete.field { abstract }",
+     "--", "delete.field { mynote }",
+     "--", "delete.field { url }",
+     "~/files/mine/text/bibliography/geoscience.bib",
+     "-o", "temp.bib"
+   ], stderr = DEVNULL)
+  bibtool.wait()
 
-  p = subprocess.Popen(["pandoc", "-t", "latex",
+  source = "@%s\n" % sys.argv[1]
+
+  pandoc = Popen(["pandoc", "-t", "html",
                         "--csl", "apa.csl",
                         "--bibliography", "temp.bib"],
                        stdout=PIPE, stdin=PIPE, stderr=PIPE)
-  input = "@%s\n" % sys.argv[1]
-  outputs = p.communicate(input.encode())
-  print(outputs[0].decode())
 
-# To copy rich text to the clipboard:
-# echo "to <b>boldly</b> go" | xclip -selection clipboard -t text/html
+  pandoc_output = pandoc.communicate(source.encode())[0]
+  print(pandoc_output.decode())
+
+  xclip = Popen(["xclip",
+                 "-selection", "clipboard",
+                 "-loops", "1",
+                 "-target", "text/html"],
+                stdin=PIPE, stdout=PIPE)
+
+  xclip.communicate(pandoc_output)
+  # print(outputs[0].decode())
 
 if __name__=="__main__":
   main()
